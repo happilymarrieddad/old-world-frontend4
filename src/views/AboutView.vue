bd41b8ef220220ef0d8dcabff9c7bb5b57194516
Nick Kotenberg <happilymarrieddad@gmail.com>
2024-04-17 11:06:04 -0600

    updated

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,5 +8,14 @@
       This project is a work in progress. You can email me at
       <email>wargamelistbuilder@gmail.com</email>
     </h3>
+    <h5>Upcoming features</h5>
+    <ol>
+      <li>Add List Printing</li>
+    </ol>
+    <h5>Release History</h5>
+    <ul>
+      <li>v1.0.1 - Fixing a few small bugs. Added ability to edit games.</li>
+      <li>v1.0.0 - Initial release. All basic functions are completed.</li>
+    </ul>
   </div>
 </template>
